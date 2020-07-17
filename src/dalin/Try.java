6c4f65a86c055a7bdf21cd568110084a361dd86f
KimChangGue <rlackdrb2904@naver.com>
2020-07-17 16:33:49 +0900

    연산의 달인

--- src/dalin/Try.java
+++ src/dalin/Try.java
@@ -7,8 +7,8 @@ public class Try {
 		ArrayList<Integer> lista = new ArrayList<Integer>();
 		ArrayList<Integer> listb = new ArrayList<Integer>();
 		ArrayList<Boolean> listc = new ArrayList<Boolean>();
-		int[][] b = new int[3][10];
 
+		int[][] b = new int[3][10];
 		Operation op = new Operation();
 		int Snop = op.opScan();
 		for (int i = 0; i < 10; i++) {
@@ -51,21 +51,26 @@ public class Try {
 			}
 		}
 		int hap = 0;
-		for (int i = 0; i < listc.size(); i++) {
-			if (listc.get(i) == true) {
-				hap = hap + 1;
-			}
-		}
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < lista.size(); j++) {
-				if (i == 0) {
-					b[i][j] = lista.get(j);
-				} else if (i == 1) {
-					b[i][j] = listb.get(j);
-				} else if (i == 2) {
-					b[i][j] = hap;
+		if (Snop != 0) {
+			for (int i = 0; i < listc.size(); i++) {
+				if (listc.get(i) == true) {
+					hap = hap + 1;
 				}
 			}
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < lista.size(); j++) {
+					if (i == 0) {
+						b[i][j] = lista.get(j);
+					} else if (i == 1) {
+						b[i][j] = listb.get(j);
+					} else if (i == 2) {
+						b[i][j] = hap;
+					}
+				}
+			}
+		}
+		else {
+			b[2][1] = 0;
 		}
 		return b;
 	}
